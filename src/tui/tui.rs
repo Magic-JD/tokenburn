2464b4dc4445a5c8e18faaf15010db4c9f3814be
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2026-06-21 19:29:39 +0100

    Overflow can cause an overflow length sleep. As a workaround, only sleeping if the maximum amount of time has not already passed.

--- src/tui/tui.rs
+++ src/tui/tui.rs
@@ -13,20 +13,24 @@ use ratatui::{
 };
 use std::sync::mpsc;
 use std::{io, thread, time};
+use std::cmp::max;
 
 impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         let config = Config::get_config();
+        let max_sleep = 1000 / config.frames_per_second as u64;
         let mut calculator = Calculator::new();
         let (sender, receiver) = mpsc::channel();
         thread::spawn(move || KeyListener::listen(sender));
         while !receiver.try_recv().is_ok() {
             let start = time::SystemTime::now();
-            self.cost_per_minute = 0.0f32;//calculator.current_cost_per_minute();
+            self.cost_per_minute = calculator.current_cost_per_minute();
             self.animation.set_state(self.cost_per_minute);
             terminal.draw(|frame| self.draw(frame))?;
-            let duration = start.elapsed().unwrap();
-            thread::sleep(time::Duration::from_millis((1000 / config.frames_per_second as u64) - duration.as_millis() as u64));
+            let time_passed = start.elapsed().unwrap().as_millis() as u64;
+            if time_passed < max_sleep {
+                thread::sleep(time::Duration::from_millis(max_sleep - time_passed));
+            }
         }
         Ok(())
     }
