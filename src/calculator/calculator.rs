5332eb1eb94b0b04a8d1906804c2a280924e15f4
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2026-06-24 23:08:02 +0100

    Stop it querying the fs 12 times a second

--- src/calculator/calculator.rs
+++ src/calculator/calculator.rs
@@ -1,3 +1,4 @@
+use std::time::SystemTime;
 use crate::calculator::log_tracker::LogTracker;
 use crate::calculator::price_calculator::PriceCalculator;
 use crate::calculator::smoothing::Smoothing;
@@ -6,6 +7,7 @@ pub struct Calculator {
     price_calc: PriceCalculator,
     log_tracker: LogTracker,
     smoothing: Smoothing,
+    last_checked: SystemTime,
 }
 impl Calculator {
     pub fn new() -> Self {
@@ -13,10 +15,16 @@ impl Calculator {
             price_calc: PriceCalculator::new(),
             log_tracker: LogTracker::new(),
             smoothing: Smoothing::new(),
+            last_checked: SystemTime::now(),
         }
     }
 
     pub fn current_cost_per_minute(&mut self) -> f32 {
+        let time = SystemTime::now();
+        if time.duration_since(self.last_checked).unwrap().as_millis() < 1000 {
+            return self.smoothing.update_and_retrieve(0.0) * 60f32
+        }
+        self.last_checked = time;
         let cost_this_sec = self
             .log_tracker
             .new_logs()
