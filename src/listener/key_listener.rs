03b2d00cc549593ca2651151f87f09f9fb02beaf
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2026-06-28 15:28:39 +0100

    Add ctrl c as a valid way to quit the program

--- src/listener/key_listener.rs
+++ src/listener/key_listener.rs
@@ -1,5 +1,5 @@
 use crossterm::event;
-use crossterm::event::KeyCode;
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use std::sync::mpsc::Sender;
 
 pub struct KeyListener {}
@@ -8,9 +8,17 @@ impl KeyListener {
     pub fn listen(sender: Sender<bool>) {
         let mut running = true;
         while running {
-            if let Some(key) = event::read().unwrap().as_key_press_event() {
-                match key.code {
-                    KeyCode::Char('q') | KeyCode::Esc => {
+            if let Some(key_event) = event::read().unwrap().as_key_press_event() {
+                match key_event {
+                    KeyEvent {
+                        code: KeyCode::Char('q') | KeyCode::Esc,
+                        ..
+                    }
+                    | KeyEvent {
+                        code: KeyCode::Char('c'),
+                        modifiers: KeyModifiers::CONTROL,
+                        ..
+                    } => {
                         running = false;
                         sender.send(true).unwrap()
                     }
